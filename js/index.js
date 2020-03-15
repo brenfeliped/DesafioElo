647ef93ac1615214b47cfdbb2fd69814af34753c
Breno Felipe <brenofeliped15@live.com>
2020-03-15 16:22:43 -0300

    ajustando

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 let botaoEnviar = document.getElementById("enviar");
+
 botaoEnviar.onclick = function () {
 
     let name = document.getElementById("nome").value;
@@ -7,23 +8,34 @@ botaoEnviar.onclick = function () {
     let firstName = completeName[0];
     let lastName = completeName[1];
     let obj;
+    let redesocial = "";
+    let ValidForm = true;
     let firstContant = document.getElementById("primeiro_contato").value;
 
 
     // validation name
     if (name == "") {
+
         alert("Por favor informe seu nome!!");
+        ValidForm = false;
+
     } else {
+
         if (lastName == "" || lastName == undefined) {
             alert("Informe um nome e sobrenome!!");
+            ValidForm = false;
         }
     }
     // validation phone
     if (phone == "") {
         alert("Por favor informe seu telefone!!");
+        ValidForm = false;
     } else {
-        if (phone.match("[0-9]{2}-[0-9]{8}") != phone) {
-            alert("O telefone dever seguir exatamente o padrão: 99-9999999\n2 números-8 números");
+
+        if (phone.match("[0-9]{2}-[0-9]{8}") != phone) { // teste se telefone segue o padrão 99-99999999
+
+            alert("O telefone dever seguir exatamente o padrão: 99-99999999\n2 números-8 números");
+            ValidForm = false;
         }
     }
 
@@ -47,32 +59,41 @@ botaoEnviar.onclick = function () {
 
         }
     } else {
+        if (document.getElementById("nao").checked) {
+            redesocial = "não"
+        }
         obj = {
             nome: name,
             telefone: phone,
             primeiro_contato: firstContant,
-            possui_rede_social: "não",
+            possui_rede_social: redesocial,
         }
     }
-    console.log(obj)
-    genJson(obj);
+
+
+    if (ValidForm) {
+        sendJson(obj);
+        alert("Formulário enviado com sucesso!");
+        document.getElementById("enviar").disabled = true;
+    }
 }
 
-function genJson(obj) {
+
+function sendJson(obj) {
     let myJson = JSON.stringify(obj);
     console.log(myJson);
     const connection = new XMLHttpRequest();
 
-    connection.open("POST", "www.google.com");
+    connection.open("POST", "http://localhost:8080");
     connection.setRequestHeader("Content-Type", "application/json");
     connection.send(myJson);
 }
-function ShowSocialMedia(divID) {
-    let display = document.getElementById(divID).style.display;
-    console.log(display);
-    if (display != "block") {
-        document.getElementById(divID).style.display = "block"
+
+
+function ShowSocialMedia(opt) {
+    if (opt == "sim") {
+        document.getElementById("rede-social-div").style.display = "block"
     } else {
-        document.getElementById(divID).style.display = "none"
+        document.getElementById("rede-social-div").style.display = "none"
     }
 }
